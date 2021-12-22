75ae1a9ce74fcd39a19a3fde5a630f2313cd4177
Umur Yasin Alper <umuryasin26@gmail.com>
2021-12-23 01:30:21 +0300

    Animations added

--- TabTabCube/Assets/Scripts/GameScripts/SquareScript.cs
+++ TabTabCube/Assets/Scripts/GameScripts/SquareScript.cs
@@ -6,16 +6,10 @@ using UnityEngine.UI;
 public class SquareScript : MonoBehaviour
 {
     public Text ScoreText;
+    public Animator animator;
     public float Vel = 3500;
-    private Rigidbody2D rigidbody;
 
     public float m_Thrust = 20f;
-
-    private bool playerIsGrounded = true;
-    private bool playerFast = true;
-
-    private System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
-    private double tabHoldTime = 100;
 
     public delegate void PointAction();
     public static event PointAction onPoint;
@@ -24,34 +18,50 @@ public class SquareScript : MonoBehaviour
     public static event SpeedAction onSpeedUp;
     public static event SpeedAction onSpeedDown;
 
-    private int PointCount = 0;
+    private Rigidbody2D rigidBody;
+
+    private bool playerIsGrounded = true;
+    private bool playerFast = true;
+
+    private System.Diagnostics.Stopwatch TabTimeWatch;
+    private System.Diagnostics.Stopwatch reverseGravityWatch;
+    private double tabHoldTime = 100;
+
+    private int pointCount = 0;
     private float horizantalVel;
     private float absoluteHorizantalVel = 10.8f;
     private float gravity = -25.8f;
+    private bool reverseGravity = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
         //Physics2D.gravity = (new Vector2(0, 5f * gravity));
         Physics2D.gravity = (new Vector2(0, 0));
         horizantalVel = absoluteHorizantalVel;
-        rigidbody.velocity = new Vector2(horizantalVel, gravity);
+        rigidBody.velocity = new Vector2(horizantalVel, gravity);
+        TabTimeWatch = new System.Diagnostics.Stopwatch();
+        reverseGravityWatch = new System.Diagnostics.Stopwatch();
         UpdateScoreText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(playerIsGrounded && playerFast && stopwatch.Elapsed.TotalMilliseconds >= tabHoldTime)
+        if (reverseGravity)
         {
-            Debug.Log("Fastest Boii");
+            rigidBody.transform.Rotate(new Vector3(1, 0, 0), 180);
+            reverseGravity = false;
+        }
+
+        if (playerIsGrounded && playerFast && TabTimeWatch.Elapsed.TotalMilliseconds >= tabHoldTime)
+        {
+            Debug.Log("Faster Boii");
             onSpeedUp?.Invoke();
         }
         else
         {
-            //Debug.Log("Fuck Boii");
             onSpeedDown?.Invoke();
         }
 
@@ -60,22 +70,36 @@ public class SquareScript : MonoBehaviour
             ReverseGravity();
         }
 
-        rigidbody.velocity = new Vector2(horizantalVel, gravity);
+        rigidBody.velocity = new Vector2(horizantalVel, gravity);
+
+     
+
     }
 
     public void ReverseGravity()
     {
+        animator.SetBool("isGround", false);
         gravity = -gravity;
+        reverseGravity = true;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
+        {
             playerIsGrounded = true;
+            animator.SetBool("isGround", true);
+        }
         else if (collision.gameObject.tag == "Obstacle")
+        {
             SpeedDown();
+        }
         else if (collision.gameObject.tag == "UpDownCollider")
+        {
             SpeedUp();
+            animator.SetBool("isGround", true);
+        }
+
     }
 
     void OnCollisionExit2D(Collision2D collision)
@@ -110,7 +134,7 @@ public class SquareScript : MonoBehaviour
     private void PointLimit()
     {
         onPoint?.Invoke();
-        PointCount++;
+        pointCount++;
         UpdateScoreText();
         //Debug.Log(" PointCount = " + PointCount);
     }
@@ -127,7 +151,7 @@ public class SquareScript : MonoBehaviour
 
     private void UpdateScoreText()
     {
-        ScoreText.text = PointCount.ToString();
+        ScoreText.text = pointCount.ToString();
     }
 
 }
